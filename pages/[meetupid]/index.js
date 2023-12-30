34d19e01f07ce61a554e1eab51a6849078c08d89
shrey0511 <112866424+shrey0511@users.noreply.github.com>
2023-12-30 15:14:01 +0530

    fallback fixed

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -31,7 +31,7 @@ export async function getStaticPaths(){
 
     client.close();
     return{
-        fallback: false,
+        fallback: 'blocking',
         paths:meetups.map((meetup)=>({
             params:{meetupid:meetup._id.toString()}
         })),
